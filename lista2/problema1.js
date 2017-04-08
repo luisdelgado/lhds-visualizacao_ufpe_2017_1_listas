87670c512349ddfc2aa4ba7d28e10975f630a3aa
luisdelgado <lhds@cin.ufpe.br>
2017-04-08 15:01:58 -0300

    Adicionando eixos

--- lista2/problema1.js
+++ lista2/problema1.js
@@ -9,3 +9,21 @@ var cScale = d3.scaleLinear().domain([0,100]).range(["grey","blue"]);
 
 var myUpdate = d3.select("body")
 	.selectAll("svg")
+
+var mySVG = d3.select("body").append("svg")
+		.attr("width", width + margin.left + margin.right + 40)
+		.attr("height", height + margin.top + margin.bottom + 30)
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+		var xAxisGroup = mySVG.append("g")
+			.attr("class","xAxis")
+			.attr("transform","translate(20,"+(height-margin.top+21)+")");
+	  	var xAxis = d3.axisBottom(xScale)
+	  		.ticks(3);
+	  		xAxisGroup.call(xAxis);
+	  	var yAxisGroup = mySVG.append("g")
+			.attr("class","yAxis")
+			.attr("transform","translate(20,18)");
+	  	var yAxis = d3.axisLeft(yScale);
+	  		yAxisGroup.call(yAxis);
