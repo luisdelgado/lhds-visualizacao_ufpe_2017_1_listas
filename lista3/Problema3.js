65a9d4759e5422f86e0c314c29f900beca9d635f
luisdelgado <lhds@cin.ufpe.br>
2017-05-07 19:28:07 -0300

    Problema 3

---
+++ lista3/Problema3.js
@@ -0,0 +1,46 @@
+//Width and height
+var w = 500;
+var h = 300;
+
+//Define path generator
+var path = d3.geoPath();
+
+//Create SVG element
+var svg = d3.select("body")
+			.append("svg")
+			.attr("width", w)
+			.attr("height", h);
+						
+d3.csv("us-ag-productivity-2004.csv", function(data){
+	var color = d3.scaleLinear().range(["#e5f5f9","#2ca25f"]);
+	color.domain([ d3.min(data, function(d) { return d.value; }),
+		d3.max(data, function(d) { return d.value; }) ]);
+	d3.json("us-states.json", function(json) {
+		for (var i = 0; i < data.length; i++) {
+			var dataState = data[i].state;
+			var dataValue = parseFloat(data[i].value);
+			for (var j = 0; j < json.features.length; j++) {
+				var jsonState = json.features[j].properties.name;
+				if (dataState == jsonState) {
+					json.features[j].properties.value = dataValue;
+					break; } } }
+				
+		//Bind data and create one path per GeoJSON feature
+		svg.selectAll("path")
+			.data(json.features)
+			.enter()
+			.append("path")
+			.attr("d", path)
+			.attr("fill", function(d) {return color(d.properties.value);});
+	
+			
+	});
+			
+})
+			
+var projection = d3.geoAlbersUsa()
+	.translate([w/2, h/2])
+	.scale([500]);
+
+var path = d3.geoPath()
+	.projection(projection);
